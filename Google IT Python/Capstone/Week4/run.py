497e267f95e2dc566fc01c297edbc36da9ccc3ee
Pranav Muktevi <66785442+SPKM2808@users.noreply.github.com>
2020-06-17 10:09:01 +0530

    All issues resolved
    run.py successfully updates website/ and uploads error free websites/fruits/

--- Google IT Python/Capstone/Week4/run.py
+++ Google IT Python/Capstone/Week4/run.py
@@ -6,15 +6,15 @@ for i in os.listdir('./supplier-data/descriptions/'):
 	buff={}
 	f=open('./supplier-data/descriptions/'+i)
 	lines=f.readlines()
-	buff['name']=lines[0].strip()
-	buff['weight']=lines[1].strip()
-	buff['description']=lines[2].strip()
-    buff["image_name"]=i.replace('txt','jpeg')
+	buff["name"]=lines[0].strip()
+	buff["weight"]=lines[1].strip().strip(' lbs')
+	buff["description"]=lines[2].strip()
+	buff["image_name"]=(i.strip('.txt'))+'.jpeg'
 	data1.append(buff)
 	f.close()
-	#print(data1)
+	print(data1)
 	x=requests.post('http://35.192.55.78/fruits/',json=buff)
-	#print(x)
+	print(x.status_code)
 
 	if x.status_code==201:
 		print('Success')
